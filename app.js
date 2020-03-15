67cd69debd26ea87b24c5766515de2abc58885ef
EC2 Default User <ec2-user@ip-172-31-27-186.us-east-2.compute.internal>
2020-03-15 23:39:00 +0000

    First Push

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+app.engine('html', require("ejs").renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req, res) {
+    res.render("index.html");
+});
+
+app.get("/mercury", function(req, res) {
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res) {
+    res.send("This will be Venus web page !");
+});
+
+//server listener 
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running express server...");
+});
